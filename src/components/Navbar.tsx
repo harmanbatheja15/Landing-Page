9ba02f4f92211c599c962b1f8f39780073bcf5ab
harmanbatheja15 <harmanbatheja15@gmail.com>
2024-10-05 16:04:02 +0530

    fixed warning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import MobileLogo from '../../public/logo.svg'
+import MobileLogo from '../assets/logo.svg'
 import RocketIcon from '../assets/rocket.svg';
 
 const Navbar = () => {
